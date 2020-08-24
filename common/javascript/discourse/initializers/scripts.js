6b3c822cb1d1ca865d8a266c6ceed3992334fdad
Aaron Anderson <aaronlynanderson@gmail.com>
2020-08-24 17:49:35 -0500

    write js

--- common/javascript/discourse/initializers/scripts.js
+++ common/javascript/discourse/initializers/scripts.js
@@ -1,3 +1,9 @@
+const button = document.getElementById("connectWallet");
+
+document.addEventListener('DOMContentReady', function () {
+    button.addEventListener('click', connectWallet);
+  });
+
 export default connectWallet = () => {
     window.ethereum.enable();
 };
