d16061c9db1869be369f731d3f936d37239b7959
Aaron Anderson <aaronlynanderson@gmail.com>
2020-08-24 18:07:42 -0500

    define addClickHandler object

--- common/javascript/discourse/initializers/scripts.js
+++ common/javascript/discourse/initializers/scripts.js
@@ -1,11 +1,22 @@
-const button = document.getElementById("connectWallet");
 
-document.addEventListener('DOMContentReady', function () {
-    button.addEventListener('click', connectWallet);
-  });
 
-export default connectWallet = (e) => {
-    e.preventDefault();
-    console.log("We hit connectWallet", e)
-    window.ethereum.enable();
-};
+// document.addEventListener('DOMContentReady', function () {
+//     button.addEventListener('click', connectWallet);
+//   });
+
+const addClickHandler = {
+    addClickHandler: () =>{
+        return document.addEventListener('DOMContentReady', function () {
+            const button = document.getElementById("connectWallet");
+            button.addEventListener('click', connectWallet);
+        })
+    },
+    connectWallet: (e) => {
+        e.preventDefault();
+        console.log("We hit connectWallet", e)
+        return window.ethereum.enable();
+    },
+}
+
+addClickHandler.addClickHandler();
+export default addClickHandler;
