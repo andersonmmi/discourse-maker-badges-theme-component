a530cf6c212874165e8cd8cfeec21136039cd1e4
Aaron Anderson <aaronlynanderson@gmail.com>
2020-09-15 19:22:54 -0500

    add descriptive comments

--- javascripts/discourse/initializers/discourse-theme-component.js
+++ javascripts/discourse/initializers/discourse-theme-component.js
@@ -6,21 +6,25 @@ const html = () => { return "Import Maker Badges"; };
 const click = (e) => {
   e.preventDefault();
 
+  // This username is the same as the "message" that gets signed
   const username = Discourse.currentUser.username;
 
+  // TODO: This method should just return the active address in metamask
   window.ethereum.send('eth_requestAccounts').then((accounts) => {
     console.log("Ethereum Accounts", accounts);
     // Message params for signing request should be username & ethAddress
 
+    // This parameter tells "window.ethereum.sendAsync" what to do: "personally sign with these params, from me"
     const sendAsyncConfig = {
       method: 'personal_sign',
       params: [
+        // message, signer
         `${username}`, window.ethereum.selectedAddress,
-        // 'username', window.ethereum.selectedAddress
       ],
       from: window.ethereum.selectedAddress,
     };
 
+    // TODO: This callback should should give us back the signature if it's successful 
     const sendAsyncCallback = ((error, response) => {
       if (error) {
         // Handle error. Likely the user rejected the signature request
@@ -29,22 +33,22 @@ const click = (e) => {
         return;
       }
 
-      // Prepare message for delivery to lambda function
-      const message = {
+      // Send Lambda the user's discourse username, ethereum address, and signature
+      const data = {
         username: username,
         address: window.ethereum.selectedAddress,
         signature: response.result,
       }
-      console.log("Calling lambda function with message:", message);
+      console.log("Sending lambda function this data:", data);
 
-      // Pass message to lambda function, unlocking any badges user has earned
+      // Pass data to lambda function, unlocking any badges user has earned
       const requestOptions = {
         method: 'GET',
         mode: 'no-cors',
       };
 
-      // TODO: This code doesn't execute here, move it.
-      fetch(matchProductionHost() ? ThemeConfig.production.lambdaUrl(message) : ThemeConfig.digitalOcean.lambdaUrl(message), requestOptions)
+      // TODO: This method should send the data to the lambda service
+      fetch(matchProductionHost() ? ThemeConfig.production.lambdaUrl(data) : ThemeConfig.digitalOcean.lambdaUrl(data), requestOptions)
         .then(response => response.json())
         .then(resolved => {
           console.log("resolved",resolved);
@@ -55,7 +59,7 @@ const click = (e) => {
 
     });
 
-    // TODO: This code doesn't "send" anything, it signs the message and returns the signed message.
+    // TODO: This method doesn't "send" anything, it signs the message and returns the signed message.
     window.ethereum.sendAsync(sendAsyncConfig, sendAsyncCallback);
   });
 }
