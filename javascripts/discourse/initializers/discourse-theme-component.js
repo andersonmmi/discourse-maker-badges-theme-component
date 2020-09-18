2a5234213d58ecc8a3be0bda93011ba9a7b03196
Jozi <koder@creativekode.com>
2020-09-17 21:45:50 -0400

    in and out of the round about

--- javascripts/discourse/initializers/discourse-theme-component.js
+++ javascripts/discourse/initializers/discourse-theme-component.js
@@ -26,41 +26,43 @@ const click = (e) => {
 
     // TODO: This callback should should give us back the signature if it's successful
     const sendAsyncCallback = ((error, response) => {
-      return new Promise(async (resolve, reject) => {
-        if (error) {
-          // Handle error. Likely the user rejected the signature request
-          console.error("Error with signing message:", error);
-          document.getElementById('badge-error').innerText = error;
-          return;
-        }
+      if (error) {
+        // Handle error. Likely the user rejected the signature request
+        console.error("Error with signing message:", error);
+        document.getElementById('badge-error').innerText = error;
+        return;
+      }
 
-        try {
-          // Send Lambda the user's discourse username, ethereum address, and signature
-          const data = {
-            username: username,
-            address: window.ethereum.selectedAddress,
-            signature: response.result,
-          }
-          console.log("Sending lambda function this data:", data);
-
-          // TODO: This method should send the data to the lambda service
-          const properURL = matchProductionHost() ? ThemeConfig.production.lambdaUrl(data) : ThemeConfig.digitalOcean.lambdaUrl(data);
-          const resData  = await fetch(properURL, { method: 'GET', mode: 'no-cors' });
-          const json     = await resData.json();
-
-          document.getElementById('badge-error').innerText = json.errors;
-
-          resolve(json);
-        } catch(error) {
-          console.log('try/catch', error);
-          reject(error)
-        }
-      });
+      return callLambda(response);
     });
 
     // TODO: This method doesn't "send" anything, it signs the message and returns the signed message.
-    window.ethereum.sendAsync(sendAsyncConfig, ( async (error, result) => { await sendAsyncCallback(error, result); } ) );
+    window.ethereum.sendAsync(sendAsyncConfig, sendAsyncCallback);
 
+    const callLambda = async (response) => {
+      try {
+        // Send Lambda the user's discourse username, ethereum address, and signature
+        const data = {
+          username: username,
+          address: window.ethereum.selectedAddress,
+          signature: response.result,
+        }
+        console.log("Sending lambda function this data:", data);
+
+        // TODO: This method should send the data to the lambda service
+        const properURL = matchProductionHost() ? ThemeConfig.production.lambdaUrl(data) : ThemeConfig.digitalOcean.lambdaUrl(data);
+        const resData  = await fetch(properURL, { method: 'GET', mode: 'no-cors' });
+        const json     = await resData.json();
+
+        document.getElementById('badge-error').innerText = json.errors;
+
+        return json;
+
+      } catch(error) {
+        console.log('try/catch', error);
+        return error;
+      }
+    }
   });
 }
 
