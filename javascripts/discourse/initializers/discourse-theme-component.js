2d5caf336ed885229158789f9ad04262c94388bf
Jozi <koder@creativekode.com>
2020-09-17 20:31:06 -0400

    fixed merge conflic, working code

--- javascripts/discourse/initializers/discourse-theme-component.js
+++ javascripts/discourse/initializers/discourse-theme-component.js
@@ -6,32 +6,23 @@ const html = () => { return "Import Maker Badges"; };
 const click = (e) => {
   e.preventDefault();
 
-  // This method unlocks and returns the active address in metamask
-  window.ethereum.send('eth_requestAccounts').then((accounts) => {
-    console.log("Ethereum Accounts", accounts);
-  }); 
-
   // This username is the same as the "message" that gets signed
   const username = Discourse.currentUser.username;
 
-  // This parameter tells "window.ethereum.sendAsync" what to do: "personally sign with these params, from me"
-  const sendAsyncConfig = {
-    method: 'personal_sign',
-    params: [
-      // Message params for signing request are username & ethAddress
-      `${username}`, window.ethereum.selectedAddress,
-    ],
-    from: window.ethereum.selectedAddress,
-  };
+  // TODO: This method should just return the active address in metamask
+  window.ethereum.send('eth_requestAccounts').then((accounts) => {
+    console.log("Ethereum Accounts", accounts);
+    // Message params for signing request should be username & ethAddress
 
-  // This callback should should give us back the signature if it's successful 
-  const sendAsyncCallback = ((error, response) => {
-    if (error) {
-      // Handle error. Likely the user rejected the signature request
-      console.error("Error with signing message:", error);
-      document.getElementById('badge-error').innerText = error;
-      return;
-    }
+    // This parameter tells "window.ethereum.sendAsync" what to do: "personally sign with these params, from me"
+    const sendAsyncConfig = {
+      method: 'personal_sign',
+      params: [
+        // message, signer
+        `${username}`, window.ethereum.selectedAddress,
+      ],
+      from: window.ethereum.selectedAddress,
+    };
 
     // TODO: This callback should should give us back the signature if it's successful
     const sendAsyncCallback = ((error, response) => {
@@ -42,35 +33,34 @@ const click = (e) => {
         return;
       }
 
-    // Pass data to lambda function, unlocking any badges user has earned
-    const requestOptions = {
-      method: 'GET',
-      mode: 'no-cors',
-    };
+      // Send Lambda the user's discourse username, ethereum address, and signature
+      const data = {
+        username: username,
+        address: window.ethereum.selectedAddress,
+        signature: response.result,
+      }
+      console.log("Sending lambda function this data:", data);
 
-    // TODO: This method should send the data to the lambda service
-    // BUG: This block results in: ngrok 200 ok, success: false, errors: [undefined]
-    fetch(matchProductionHost() ? ThemeConfig.production.lambdaUrl(data) : ThemeConfig.digitalOcean.lambdaUrl(data), requestOptions)
-    // .then(async response => {
-    //   console.log(response.json());
-    //   return await response.json();} )
-    .then(async (json) => {
-      const x = json.json();
-      console.log("json", x);
-      // document.getElementById('badge-error').innerText = json.errors;
-    })
-    .catch(error => { console.log('fetch error:', error); });
-  });
+      // Pass data to lambda function, unlocking any badges user has earned
+      const requestOptions = {
+        method: 'GET',
+        mode: 'no-cors',
+      };
 
-    // TODO: This method should send the data to the lambda service
-    fetch(matchProductionHost() ? ThemeConfig.production.lambdaUrl(data) : ThemeConfig.digitalOcean.lambdaUrl(data), requestOptions)
-      .then(response => response.json())
-      .then(resolved => {
-        console.log("resolved",resolved);
-        document.getElementById('badge-error').innerText = resolved.errors; })
-      .catch(error => { console.log(error);
+      // TODO: This method should send the data to the lambda service
+      fetch(matchProductionHost() ? ThemeConfig.production.lambdaUrl(data) : ThemeConfig.digitalOcean.lambdaUrl(data), requestOptions)
+        .then(response => response.json())
+        .then(resolved => {
+          console.log("resolved",resolved);
+          document.getElementById('badge-error').innerText = resolved.errors; })
+        .catch(error => { console.log(error);
+      });
+
     });
 
+    // TODO: This method doesn't "send" anything, it signs the message and returns the signed message.
+    window.ethereum.sendAsync(sendAsyncConfig, sendAsyncCallback);
+  });
 }
 
 export default {
