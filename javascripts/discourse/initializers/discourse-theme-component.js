325e4d32ad307f3389f6fd7a69c837538b2fc646
Aaron Anderson <aaronlynanderson@gmail.com>
2020-09-17 19:12:01 -0500

    declare data in scope

--- javascripts/discourse/initializers/discourse-theme-component.js
+++ javascripts/discourse/initializers/discourse-theme-component.js
@@ -1,7 +1,5 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
-let data = {};
-
 const matchProductionHost = () => {return window.location.host === "forum.makerdao.com"};
 
 const html = () => { return "Import Maker Badges"; };
@@ -36,7 +34,7 @@ const click = (e) => {
     }
 
     // Send Lambda the user's discourse username, ethereum address, and signature
-    data = {
+    const data = {
       username: username,
       address: window.ethereum.selectedAddress,
       signature: response.result,
