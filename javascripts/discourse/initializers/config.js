c79fecadd0167bc26cafbb7b3a25fcb14bc36530
Aaron Anderson <aaronlynanderson@gmail.com>
2020-09-18 17:27:28 -0500

    destroy the very memory of axios

--- javascripts/discourse/initializers/config.js
+++ javascripts/discourse/initializers/config.js
@@ -1,5 +1,3 @@
-// import axios from "./axios";
-
 const ThemeConfig = {
     development: {
         lambdaUrl: (message)=>{return `https://075d145e319a.ngrok.io/dev/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`;}
@@ -16,6 +14,5 @@ export default {
     name: 'maker-badge',
     initialize(){
         globalThis.ThemeConfig = ThemeConfig;
-        // globalThis.axios = axios;
     }
 }
