d9a305b5ed03cbca7e54f1293467d71a77765d2d
Aaron Anderson <aaronlynanderson@gmail.com>
2020-09-25 13:01:50 -0500

    test serverless cors

--- javascripts/discourse/initializers/config.js
+++ javascripts/discourse/initializers/config.js
@@ -1,9 +1,9 @@
 const ThemeConfig = {
     development: {
-        lambdaUrl: (message)=>{return `https://1i6l13wb90.execute-api.us-east-1.amazonaws.com/dev/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`;}
+        lambdaUrl: (message)=>{return `https://f44lkeezmb.execute-api.us-east-1.amazonaws.com/dev/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`;}
     },
     digitalOcean: {
-        lambdaUrl: (message)=>{return `https://web33.ngrok.io/dev/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`;}
+        lambdaUrl: (message)=>{return `https://web33.ngrok.io/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`;}
     },
     production: {
         lambdaUrl: (message)=>{return `https://3nsc1491ed.execute-api.us-east-1.amazonaws.com/dev/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`;}
